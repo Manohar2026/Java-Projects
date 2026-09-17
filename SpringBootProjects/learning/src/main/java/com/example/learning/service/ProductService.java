f0da0ef4733512d4f31267745c4f538bebd02ab6
Manohar <manohar.x03@gmail.com>
2026-09-17 11:47:36 +0530

    Product service structure is ready

--- SpringBootProjects/learning/src/main/java/com/example/learning/service/ProductService.java
+++ SpringBootProjects/learning/src/main/java/com/example/learning/service/ProductService.java
@@ -1,7 +1,24 @@
 package com.example.learning.service;
 
+import com.example.learning.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
+
+    ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public void createProduct() {}
+
+    public void getAllProducts() {}
+
+    public void getProductById() {}
+
+    public void updateProduct() {}
+
+    public void deleteProduct() {}
 }
