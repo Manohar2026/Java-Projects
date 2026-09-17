91ad7fbbd8b5f25af9feaef418c2a59a315ceb34
Manohar <manohar.x03@gmail.com>
2026-09-17 11:50:50 +0530

    Product Controller structure is ready

--- SpringBootProjects/learning/src/main/java/com/example/learning/controller/ProductController.java
+++ SpringBootProjects/learning/src/main/java/com/example/learning/controller/ProductController.java
@@ -1,11 +1,30 @@
 package com.example.learning.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.learning.service.ProductService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/Product")
 public class ProductController {
 
+    ProductService productService;
 
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public void createProduct() {}
+
+    @GetMapping
+    public void getAllProducts() {}
+
+    @GetMapping("{id}")
+    public void getProductById() {}
+
+    @PutMapping
+    public void updateProduct() {}
+
+    @DeleteMapping
+    public void deleteProduct() {}
 }
